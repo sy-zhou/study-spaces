03c5ed6a7568b2f8a75f634d44735e8127f07adb
Sally Zhou <siyue.zhou0246@gmail.com>
2019-12-30 18:13:05 -0500

    improve display of class times

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -18,18 +18,19 @@ module.exports = function(app, db) {
     res.send(b);
   })
   app.get('/api/building/:building/:room/courses', async (req, res) => {
-    const b = buildings.find(b => b.code === req.params.building);
-    if (!b) res.status(404).send('dne');
-    const r = b.rooms.find(r => r === req.params.room);
-    // fetch data from uwaterloo api
-    let courses = await getData(`http://api.uwaterloo.ca/v2/buildings/${b.code}/${r}/courses.json?key=${API_KEY}`);
-    courses = courses.map(course => {
-      return {
-        weekdays: course.weekdays,
-        start_time: course.start_time,
-        end_time: course.end_time,
-      }
-    });
+    // const b = buildings.find(b => b.code === req.params.building);
+    // if (!b) res.status(404).send('dne');
+    // const r = b.rooms.find(r => r === req.params.room);
+    // // fetch data from uwaterloo api
+    // let courses = await getData(`http://api.uwaterloo.ca/v2/buildings/${b.code}/${r}/courses.json?key=${API_KEY}`);
+    // courses = courses.map(course => {
+    //   return {
+    //     weekdays: course.weekdays,
+    //     start_time: course.start_time,
+    //     end_time: course.end_time,
+    //   }
+    // });
+    const courses = [{"weekdays":"MWF","start_time":"13:30","end_time":"14:20"},{"weekdays":"TTh","start_time":"11:30","end_time":"12:50"},{"weekdays":"TTh","start_time":"10:00","end_time":"11:20"},{"weekdays":"TTh","start_time":"14:30","end_time":"15:50"},{"weekdays":"TTh","start_time":"13:00","end_time":"14:20"},{"weekdays":"TTh","start_time":"08:30","end_time":"09:50"},{"weekdays":"MW","start_time":"08:30","end_time":"09:50"},{"weekdays":"MW","start_time":"16:00","end_time":"17:20"},{"weekdays":"MWF","start_time":"11:30","end_time":"12:20"},{"weekdays":"MWF","start_time":"10:30","end_time":"11:20"},{"weekdays":"M","start_time":"17:30","end_time":"18:20"},{"weekdays":"M","start_time":"14:30","end_time":"15:20"},{"weekdays":"M","start_time":"12:30","end_time":"13:20"},{"weekdays":"MW","start_time":"16:00","end_time":"17:20"}];
 
     res.send(courses);
   })
--- frontend/src/Timetable.jsx
+++ frontend/src/Timetable.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Table from 'react-bootstrap/Table';
 import './Timetable.css';
 
 class Timetable extends Component {
@@ -6,6 +7,30 @@ class Timetable extends Component {
     times: []
   };
 
+  isTenMinutesApart(endTime, startTime) {
+    const milliseconds = ((new Date("2017-05-02T" + startTime)) - (new Date("2017-05-02T" + endTime)));
+    return (milliseconds / (60000)) == 10;
+  }
+
+  formatTimesOnWeekday(weekday) {
+    // deep copy times to temp
+    let temp = this.state.times.filter(time => time.weekdays.match(new RegExp(weekday)))
+      .sort((a,b) => (a.start_time > b.start_time) ? 1 : ((b.start_time > a.start_time) ? -1 : 0))
+      .reduce((a, b) => { if (a.length == 0 || a[a.length - 1].start_time != b.start_time) a.push(b); return a; }, [])
+      .map(a => Object.assign({}, a));
+    if (temp === undefined || temp.length == 0) return [];
+    let final = [temp[0]];
+    for (let i = 1; i < temp.length; ++i) {
+      console.log(temp);
+      if (this.isTenMinutesApart(final[final.length - 1].end_time, temp[i].start_time)) {
+        final[final.length - 1].end_time = temp[i].end_time;
+      } else {
+        final.push(temp[i]);
+      }
+    }
+    return final.map(time => <div>{time.start_time} - {time.end_time}</div> )
+  }
+  
   callAPI() {
     const { building, room } = this.props;
     fetch(`http://localhost:8000/api/building/${building}/${room}/courses`)
@@ -20,6 +45,26 @@ class Timetable extends Component {
   render() {
     return (
       <div className="timetable">
+        <Table>
+          <thead>
+            <tr>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+              <th>Thursday</th>
+              <th>Friday</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{ this.formatTimesOnWeekday("M") }</td>
+              <td>{ this.formatTimesOnWeekday("T(?!h)") }</td>
+              <td>{ this.formatTimesOnWeekday("W") }</td>
+              <td>{ this.formatTimesOnWeekday("Th") }</td>
+              <td>{ this.formatTimesOnWeekday("F") }</td>
+            </tr>
+          </tbody>
+        </Table>
         {
           this.state.times.map(time => {
             return (<div>{time.weekdays} {time.start_time} {time.end_time} </div>);
