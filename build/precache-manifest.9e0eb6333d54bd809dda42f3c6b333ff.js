b4cbb3271c4f68f2d8d4c5a89859f47e636497c4
Sally Zhou <siyue.zhou0246@gmail.com>
2020-01-18 14:20:58 -0500

    move more things

---
+++ build/precache-manifest.9e0eb6333d54bd809dda42f3c6b333ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304d5f4b8ad840bc31043d37c6caa96c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b9509a0c389290fb05",
+    "url": "/static/css/2.177010a2.chunk.css"
+  },
+  {
+    "revision": "db1d8f6adcca29c0a918",
+    "url": "/static/css/main.e2d99532.chunk.css"
+  },
+  {
+    "revision": "e7b9509a0c389290fb05",
+    "url": "/static/js/2.d8d6f4ad.chunk.js"
+  },
+  {
+    "revision": "738716617e8efb6d98675f77600eaee3",
+    "url": "/static/js/2.d8d6f4ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db1d8f6adcca29c0a918",
+    "url": "/static/js/main.3c8db786.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  }
+]);
--- server.js
+++ server.js
@@ -7,20 +7,19 @@ const app = express();
 app.use(cors());
 require('./routes')(app, {});
 
-// static file declaration
-app.use(express.static(path.join(__dirname, './frontend/build')));
-
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // production mode
+  app.use(express.static(path.join(__dirname, 'build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-} else {
-  // build mode
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/public', 'index.html'));
-  });
-}
+// } else {
+//   // build mode
+//   app.use(express.static(path.join(__dirname, './frontend/build')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './frontend/public', 'index.html'));
+//   });
+// }
 
 // start server
 app.listen(port);
